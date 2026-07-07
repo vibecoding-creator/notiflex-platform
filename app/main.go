93bde4e8edec70d06fa83a1f0893ec6ec8656929
vibecoding-creator <vibecodecreator@gmail.com>
2026-07-08 00:47:28 +0900

    Reapply "feat: /version 엔드포인트 추가 및 v0.1.1 배포"
    This reverts commit 7acb89a404de27dcd3ad5e618bba313a653c252e.

--- app/main.go
+++ app/main.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+const version = "0.1.1"
+
 var (
 	counter  int
 	counterM sync.Mutex
@@ -40,9 +42,16 @@ func id(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `{"id":%d,"pod":"%s","timestamp":"%s"}`, currentID, podName, time.Now().UTC().Format(time.RFC3339))
 }
 
+func versionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `{"version":"%s","timestamp":"%s"}`, version, time.Now().UTC().Format(time.RFC3339))
+}
+
 func main() {
 	http.HandleFunc("/health", health)
 	http.HandleFunc("/id", id)
+	http.HandleFunc("/version", versionHandler)
 
 	log.Println("Notiflex API server starting on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
